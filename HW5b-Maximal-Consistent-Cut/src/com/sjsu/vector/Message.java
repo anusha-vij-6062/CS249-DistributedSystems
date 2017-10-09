ce3b23f2134d49a527c1663e9835a4ce6d358cdf
anusha-vij-6062 <32208620+anusha-vij-6062@users.noreply.github.com>
2017-10-08 17:50:47 -0700

    Added parameter cut for input cut

--- HW5b-Maximal-Consistent-Cut/src/com/sjsu/vector/Message.java
+++ HW5b-Maximal-Consistent-Cut/src/com/sjsu/vector/Message.java
@@ -3,10 +3,19 @@ package com.sjsu.vector;
 public class Message {
     MessageType messageType;
     VectorClock vc;
+    int []cut;
 
     public Message(MessageType mt, VectorClock vc) {
         this.messageType=mt;
         this.vc = vc;
     }
 
+    public Message(MessageType mt,int[]k){
+        this.messageType=mt;
+        this.cut=k;
+    }
+
+    public int[] getCut() {
+        return cut;
+    }
 }
